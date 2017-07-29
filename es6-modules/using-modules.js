59bca24b4466ae4035ff8104f9f0cd1ee59f5f97
Javier Gamarra <nhpatt@gmail.com>
2017-07-29 14:49:52 +0200

    move scripts to es6 folders

--- es6-modules/using-modules.js
+++ es6-modules/using-modules.js
@@ -1,9 +1,11 @@
 import {foo} from './modules'
+import asdasd from './modules'
 import { default as awesome } from './modules'
 import awesome2 from './modules'
 import {bar as other} from './modules'
 
 foo();
+console.log(asdasd);
 
 console.log(awesome);
 console.log(awesome2);
